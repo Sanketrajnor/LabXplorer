42a05c68820ca6c1377543b998c5354b7ff4c579
Sanketrajnor <sanketrajnor@gmail.com>
2025-01-19 16:44:07 +0530

    Initial Commit

---
+++ openCV/build/intermediates/cxx/Debug/2r6j3x6e/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\PC\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\PC\\AndroidStudioProjects\\LabXplorer\\openCV\\.cxx\\Debug\\2r6j3x6e\\arm64-v8a" ^
+  opencv_jni_shared
